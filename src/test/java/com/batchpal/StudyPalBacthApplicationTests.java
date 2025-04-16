858775b8926dd653751ed099d7ca7d54ed3afd1e
unikal1 <jack8226123@gmail.com>
2025-04-16 15:04:21 +0900

    init commit

---
+++ src/test/java/com/batchpal/StudyPalBacthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batchpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyPalBacthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
